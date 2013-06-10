f0a946a43e7658b3de448aa902459ba7c54ec8ef
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-06-10 22:13:04 +0100

    Added tests for a zip file containing a hierarchy of subdirectories and files

--- DotNetZipExploration/DotNetZipTests.cs
+++ DotNetZipExploration/DotNetZipTests.cs
@@ -16,81 +16,164 @@ namespace DotNetZipExploration
         public void GivenAStreamContainingAZipFileContainingASingleFile_WeCanInspectTheContentsOfTheZipFile()
         {
             // Arrange
-            var zipFileStream = ReadZipFile("OneFile.zip");
+            using (var zipFileStream = ReadZipFile("OneFile.zip"))
+            {
+                // Act
+                var zipFile = ZipFile.Read(zipFileStream);
 
-            // Act
-            var zipFile = ZipFile.Read(zipFileStream);
-
-            // Assert
-            Assert.That(zipFile.EntryFileNames.Count, Is.EqualTo(1));
-            Assert.That(zipFile.EntryFileNames.ElementAt(0), Is.EqualTo("File1.txt"));
-            Assert.That(zipFile.Entries.Count, Is.EqualTo(1));
-            Assert.That(zipFile[0].FileName, Is.EqualTo("File1.txt"));
+                // Assert
+                Assert.That(zipFile.EntryFileNames.Count, Is.EqualTo(1));
+                Assert.That(zipFile.EntryFileNames.ElementAt(0), Is.EqualTo("File1.txt"));
+                Assert.That(zipFile.Entries.Count, Is.EqualTo(1));
+                Assert.That(zipFile[0].FileName, Is.EqualTo("File1.txt"));
+            }
         }
 
         [Test]
         public void GivenAStreamContainingAZipFileContainingThreeFiles_WeCanInspectTheContentsOfTheZipFile()
         {
             // Arrange
-            var zipFileStream = ReadZipFile("ThreeFiles.zip");
+            using (var zipFileStream = ReadZipFile("ThreeFiles.zip"))
+            {
+                // Act
+                var zipFile = ZipFile.Read(zipFileStream);
 
-            // Act
-            var zipFile = ZipFile.Read(zipFileStream);
-
-            // Assert
-            Assert.That(zipFile.EntryFileNames.Count, Is.EqualTo(3));
-            Assert.That(zipFile.EntryFileNames.Contains("File1.txt"), Is.True);
-            Assert.That(zipFile.EntryFileNames.Contains("File2.txt"), Is.True);
-            Assert.That(zipFile.EntryFileNames.Contains("File3.txt"), Is.True);
-            Assert.That(zipFile.Entries.Count, Is.EqualTo(3));
+                // Assert
+                Assert.That(zipFile.EntryFileNames.Count, Is.EqualTo(3));
+                Assert.That(zipFile.EntryFileNames.Contains("File1.txt"), Is.True);
+                Assert.That(zipFile.EntryFileNames.Contains("File2.txt"), Is.True);
+                Assert.That(zipFile.EntryFileNames.Contains("File3.txt"), Is.True);
+                Assert.That(zipFile.Entries.Count, Is.EqualTo(3));
+            }
         }
 
         [Test]
         public void GivenAStreamContainingAZipFileContainingASingleFile_WeCanExtractTheFileIntoAMemoryStream()
         {
             // Arrange
-            var zipFileStream = ReadZipFile("OneFile.zip");
-            var zipFile = ZipFile.Read(zipFileStream);
-
-            // Act
-            var zipEntry = zipFile[0];
-            string fileContents;
-            using (var memoryStream = new MemoryStream())
+            using (var zipFileStream = ReadZipFile("OneFile.zip"))
             {
-                zipEntry.Extract(memoryStream);
-                var bytes = memoryStream.ToArray();
-                fileContents = Encoding.UTF8.GetString(bytes);
-            }
+                var zipFile = ZipFile.Read(zipFileStream);
 
-            // Assert
-            Assert.That(fileContents, Is.EqualTo("File1.txt"));
+                // Act
+                var zipEntry = zipFile[0];
+                string fileContents;
+                using (var memoryStream = new MemoryStream())
+                {
+                    zipEntry.Extract(memoryStream);
+                    var bytes = memoryStream.ToArray();
+                    fileContents = Encoding.UTF8.GetString(bytes);
+                }
+
+                // Assert
+                Assert.That(fileContents, Is.EqualTo("File1.txt"));
+            }
         }
 
         [Test]
         public void GivenAStreamContainingAZipFileContainingThreeFiles_WeCanExtractEachFileIntoAMemoryStream()
         {
             // Arrange
-            var zipFileStream = ReadZipFile("ThreeFiles.zip");
-            var zipFile = ZipFile.Read(zipFileStream);
-
-            // Act
-            var fileNamesToFileContents = new Dictionary<string, string>();
-            foreach (var zipEntry in zipFile)
+            using (var zipFileStream = ReadZipFile("ThreeFiles.zip"))
             {
-                using (var memoryStream = new MemoryStream())
-                {
-                    zipEntry.Extract(memoryStream);
-                    var bytes = memoryStream.ToArray();
-                    var fileContents = Encoding.UTF8.GetString(bytes);
-                    fileNamesToFileContents[zipEntry.FileName] = fileContents;
-                }
-            }
+                var zipFile = ZipFile.Read(zipFileStream);
 
-            // Assert
-            Assert.That(fileNamesToFileContents.Count(), Is.EqualTo(3));
-            Assert.That(fileNamesToFileContents["File1.txt"], Is.EqualTo("File1.txt"));
-            Assert.That(fileNamesToFileContents["File2.txt"], Is.EqualTo("File2.txt"));
-            Assert.That(fileNamesToFileContents["File3.txt"], Is.EqualTo("File3.txt"));
+                // Act
+                var fileNamesToFileContents = new Dictionary<string, string>();
+                foreach (var zipEntry in zipFile)
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        zipEntry.Extract(memoryStream);
+                        var bytes = memoryStream.ToArray();
+                        var fileContents = Encoding.UTF8.GetString(bytes);
+                        fileNamesToFileContents[zipEntry.FileName] = fileContents;
+                    }
+                }
+
+                // Assert
+                Assert.That(fileNamesToFileContents.Count(), Is.EqualTo(3));
+                Assert.That(fileNamesToFileContents["File1.txt"], Is.EqualTo("File1.txt"));
+                Assert.That(fileNamesToFileContents["File2.txt"], Is.EqualTo("File2.txt"));
+                Assert.That(fileNamesToFileContents["File3.txt"], Is.EqualTo("File3.txt"));
+            }
+        }
+
+        [Test]
+        public void GivenAStreamContainingAZipFileContainingAHierarchyOfFilesAndSubDirectories_WeCanInspectTheContentsOfTheZipFile()
+        {
+            // Arrange
+            using (var zipFileStream = ReadZipFile("Hierarchy.zip"))
+            {
+                // Act
+                var zipFile = ZipFile.Read(zipFileStream);
+
+                // Assert
+                Assert.That(zipFile.EntriesSorted.Count(e => e.IsDirectory), Is.GreaterThan(0));
+                Assert.That(zipFile.EntriesSorted.Count(e => !e.IsDirectory), Is.GreaterThan(0));
+            }
+        }
+
+        [Test]
+        public void
+            GivenAStreamContainingAZipFileContainingAHierarchyOfFilesAndSubDirectories_WeCanCreateADictionaryOfSubDirectoriesToFiles
+            ()
+        {
+            // Arrange
+            using (var zipFileStream = ReadZipFile("Hierarchy.zip"))
+            {
+                // Act
+                var zipFile = ZipFile.Read(zipFileStream);
+                var subDirectoriesToFiles =
+                    zipFile.EntriesSorted
+                           .Where(e => e.IsDirectory)
+                           .ToDictionary(
+                               e => e.FileName,
+                               e => new List<string>());
+                var fileEntries = zipFile.EntriesSorted.Where(e => !e.IsDirectory);
+                foreach (var e in fileEntries)
+                {
+                    var lastSlash = e.FileName.LastIndexOf('/');
+                    var subDirectory = lastSlash >= 0 ? e.FileName.Substring(0, lastSlash + 1) : string.Empty;
+                    var fileName = lastSlash >= 0 ? e.FileName.Substring(lastSlash + 1) : e.FileName;
+                    if (subDirectoriesToFiles.ContainsKey(subDirectory))
+                    {
+                        subDirectoriesToFiles[subDirectory].Add(fileName);
+                    }
+                    else
+                    {
+                        subDirectoriesToFiles.Add(subDirectory, new List<string> {fileName});
+                    }
+                }
+
+                // Assert
+                Assert.That(subDirectoriesToFiles.ContainsKey(""), Is.True);
+                Assert.That(subDirectoriesToFiles[""].Contains("File1.txt"), Is.True);
+                Assert.That(subDirectoriesToFiles[""].Contains("File2.txt"), Is.True);
+                Assert.That(subDirectoriesToFiles[""].Contains("File3.txt"), Is.True);
+
+                Assert.That(subDirectoriesToFiles.ContainsKey("SubDirectoryA/"), Is.True);
+                Assert.That(subDirectoriesToFiles["SubDirectoryA/"].Contains("SubDirectoryA.txt"), Is.True);
+
+                Assert.That(subDirectoriesToFiles.ContainsKey("SubDirectoryA/SubDirectoryA-1/"), Is.True);
+                Assert.That(subDirectoriesToFiles["SubDirectoryA/SubDirectoryA-1/"].Contains("SubDirectoryA-1_File1.txt"), Is.True);
+                Assert.That(subDirectoriesToFiles["SubDirectoryA/SubDirectoryA-1/"].Contains("SubDirectoryA-1_File2.txt"), Is.True);
+
+                Assert.That(subDirectoriesToFiles.ContainsKey("SubDirectoryA/SubDirectoryA-2/"), Is.True);
+                Assert.That(subDirectoriesToFiles["SubDirectoryA/SubDirectoryA-2/"].Contains("SubDirectoryA-2_File1.txt"), Is.True);
+                Assert.That(subDirectoriesToFiles["SubDirectoryA/SubDirectoryA-2/"].Contains("SubDirectoryA-2_File2.txt"), Is.True);
+
+                Assert.That(subDirectoriesToFiles.ContainsKey("SubDirectoryB/"), Is.True);
+                Assert.That(subDirectoriesToFiles["SubDirectoryB/"].Contains("SubDirectoryB.txt"), Is.True);
+
+                Assert.That(subDirectoriesToFiles.ContainsKey("SubDirectoryB/SubDirectoryB-1/"), Is.True);
+                Assert.That(subDirectoriesToFiles["SubDirectoryB/SubDirectoryB-1/"].Contains("SubDirectoryB-1_File1.txt"), Is.True);
+                Assert.That(subDirectoriesToFiles["SubDirectoryB/SubDirectoryB-1/"].Contains("SubDirectoryB-1_File2.txt"), Is.True);
+
+                Assert.That(subDirectoriesToFiles.ContainsKey("SubDirectoryB/SubDirectoryB-2/"), Is.True);
+                Assert.That(subDirectoriesToFiles["SubDirectoryB/SubDirectoryB-2/"].Contains("SubDirectoryB-2_File1.txt"), Is.True);
+                Assert.That(subDirectoriesToFiles["SubDirectoryB/SubDirectoryB-2/"].Contains("SubDirectoryB-2_File2.txt"), Is.True);
+            }
         }
 
         private static Stream ReadZipFile(string fileName)
