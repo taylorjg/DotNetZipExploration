b319578d2746b69fd8b32986204eef9991a2e319
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2013-06-12 17:37:43 +0100

    Added a new test re creating a tree of directories/files

--- DotNetZipExploration/DotNetZipTests.cs
+++ DotNetZipExploration/DotNetZipTests.cs
@@ -169,11 +169,144 @@ namespace DotNetZipExploration
             }
         }
 
+        [Test]
+        public void GivenAStreamContainingAZipFileContainingAHierarchyOfFilesAndSubDirectories_WeCanCreateATreeOfDirectoriesAndFiles()
+        {
+            // Arrange
+            using (var zipFileStream = ReadZipFile("Hierarchy.zip"))
+            {
+                // Act
+                var zipFile = ZipFile.Read(zipFileStream);
+                var subDirectoriesToFiles = new Dictionary<string, IList<string>>();
+                var fileEntries = zipFile.EntriesSorted.Where(e => !e.IsDirectory);
+                foreach (var fileEntry in fileEntries)
+                {
+                    var lastSlash = fileEntry.FileName.LastIndexOf('/');
+                    var subDirectory = lastSlash >= 0 ? fileEntry.FileName.Substring(0, lastSlash) : string.Empty;
+                    var fileName = lastSlash >= 0 ? fileEntry.FileName.Substring(lastSlash + 1) : fileEntry.FileName;
+                    if (subDirectoriesToFiles.ContainsKey(subDirectory))
+                    {
+                        subDirectoriesToFiles[subDirectory].Add(fileName);
+                    }
+                    else
+                    {
+                        subDirectoriesToFiles.Add(subDirectory, new List<string> { fileName });
+                    }
+                }
+
+                var root = new MyZipDirectory {DirectoryName = string.Empty};
+                foreach (var kvp in subDirectoriesToFiles)
+                {
+                    var treeNode = FindTreeNodeForFullDirectoryPath(root, kvp.Key);
+                    foreach (var fileName in kvp.Value)
+                    {
+                        treeNode.Files.Add(new MyZipFile {FileName = fileName});
+                    }
+                }
+
+                // Assert
+                Assert.That(root.SubDirectories.Count, Is.EqualTo(2));
+                Assert.That(root.SubDirectories.Select(sd => sd.DirectoryName), Is.EquivalentTo(new[] {"SubDirectoryA", "SubDirectoryB"}));
+                Assert.That(root.Files.Select(f => f.FileName), Is.EquivalentTo(new[] {"File1.txt", "File2.txt", "File3.txt"}));
+
+                var subDirectoryA = root.SubDirectories.Single(d => d.DirectoryName == "SubDirectoryA");
+                Assert.That(subDirectoryA.SubDirectories.Select(sd => sd.DirectoryName), Is.EquivalentTo(new[] { "SubDirectoryA-1", "SubDirectoryA-2" }));
+                Assert.That(subDirectoryA.Files.Select(f => f.FileName), Is.EquivalentTo(new[] { "SubDirectoryA.txt" }));
+
+                var subDirectoryA_1 = subDirectoryA.SubDirectories.Single(d => d.DirectoryName == "SubDirectoryA-1");
+                Assert.That(subDirectoryA_1.SubDirectories.Select(sd => sd.DirectoryName), Is.EquivalentTo(new string[0]));
+                Assert.That(subDirectoryA_1.Files.Select(f => f.FileName), Is.EquivalentTo(new[] {"SubDirectoryA-1_File1.txt", "SubDirectoryA-1_File2.txt"}));
+
+                var subDirectoryA_2 = subDirectoryA.SubDirectories.Single(d => d.DirectoryName == "SubDirectoryA-2");
+                Assert.That(subDirectoryA_2.SubDirectories.Select(sd => sd.DirectoryName), Is.EquivalentTo(new string[0]));
+                Assert.That(subDirectoryA_2.Files.Select(f => f.FileName), Is.EquivalentTo(new[] { "SubDirectoryA-2_File1.txt", "SubDirectoryA-2_File2.txt" }));
+
+                var subDirectoryB = root.SubDirectories.Single(d => d.DirectoryName == "SubDirectoryB");
+                Assert.That(subDirectoryB.SubDirectories.Select(sd => sd.DirectoryName), Is.EquivalentTo(new[] { "SubDirectoryB-1", "SubDirectoryB-2" }));
+                Assert.That(subDirectoryB.Files.Select(f => f.FileName), Is.EquivalentTo(new[] { "SubDirectoryB.txt" }));
+
+                var subDirectoryB_1 = subDirectoryB.SubDirectories.Single(d => d.DirectoryName == "SubDirectoryB-1");
+                Assert.That(subDirectoryB_1.SubDirectories.Select(sd => sd.DirectoryName), Is.EquivalentTo(new string[0]));
+                Assert.That(subDirectoryB_1.Files.Select(f => f.FileName), Is.EquivalentTo(new[] { "SubDirectoryB-1_File1.txt", "SubDirectoryB-1_File2.txt" }));
+
+                var subDirectoryB_2 = subDirectoryB.SubDirectories.Single(d => d.DirectoryName == "SubDirectoryB-2");
+                Assert.That(subDirectoryB_2.SubDirectories.Select(sd => sd.DirectoryName), Is.EquivalentTo(new string[0]));
+                Assert.That(subDirectoryB_2.Files.Select(f => f.FileName), Is.EquivalentTo(new[] { "SubDirectoryB-2_File1.txt", "SubDirectoryB-2_File2.txt" }));
+            }
+        }
+
+        private static MyZipDirectory FindTreeNodeForFullDirectoryPath(MyZipDirectory directory, string fullDirectoryPath)
+        {
+            var directoryNames = fullDirectoryPath.Split('/');
+
+            var treeNodeForPreviousLevel = directory;
+
+            foreach (var directoryName in directoryNames)
+            {
+                var treeNodeForThisLevel = FindTreeNodeForDirectory(directory, directoryName);
+                if (treeNodeForThisLevel != null)
+                {
+                    treeNodeForPreviousLevel = treeNodeForThisLevel;
+                }
+                else
+                {
+                    var newSubDirectory = new MyZipDirectory {DirectoryName = directoryName};
+                    if (treeNodeForPreviousLevel != null)
+                    {
+                        treeNodeForPreviousLevel.SubDirectories.Add(newSubDirectory);
+                        treeNodeForPreviousLevel = newSubDirectory;
+                    }
+                }
+            }
+
+            return treeNodeForPreviousLevel;
+        }
+
+        private static MyZipDirectory FindTreeNodeForDirectory(MyZipDirectory directory, string directoryName)
+        {
+            if (directory.DirectoryName == directoryName)
+            {
+                return directory;
+            }
+
+            // ReSharper disable LoopCanBeConvertedToQuery
+            foreach (var subDirectory in directory.SubDirectories)
+            {
+                var result = FindTreeNodeForDirectory(subDirectory, directoryName);
+
+                if (result != null)
+                {
+                    return result;
+                }
+            }
+            // ReSharper restore LoopCanBeConvertedToQuery
+
+            return null;
+        }
+
         private static Stream ReadZipFile(string fileName)
         {
             var fullPath = Path.Combine("TestFiles", fileName);
             var fileStream = File.OpenRead(fullPath);
             return fileStream;
         }
+    }
+
+    public class MyZipDirectory
+    {
+        public MyZipDirectory()
+        {
+            SubDirectories = new List<MyZipDirectory>();
+            Files = new List<MyZipFile>();
+        }
+
+        public string DirectoryName { get; set; }
+        public IList<MyZipDirectory> SubDirectories { get; private set; }
+        public IList<MyZipFile> Files { get; private set; }
+    }
+
+    public class MyZipFile
+    {
+        public string FileName { get; set; }
     }
 }
